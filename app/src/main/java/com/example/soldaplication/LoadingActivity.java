68453c8f2b12b3b99ec25786d876568d1b572bc1
chrisalo97 <vizcarralonso@gmail.com>
2017-09-11 20:10:13 -0500

    Creating loading transition

--- app/src/main/java/com/example/soldaplication/LoadingActivity.java
+++ app/src/main/java/com/example/soldaplication/LoadingActivity.java
@@ -2,12 +2,17 @@ package com.example.soldaplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class LoadingActivity extends AppCompatActivity {
 
+    private ImageView ic_img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        ic_img = (ImageView) findViewById(R.id.ic_sold);
     }
 }
