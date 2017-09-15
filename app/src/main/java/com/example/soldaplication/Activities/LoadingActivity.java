80c24aa19063ae68200cd6629f8687b57ac9f803
erick22se <erick1798CF@hotmail.com>
2017-09-14 20:58:28 -0500

    Adding new packages

--- app/src/main/java/com/example/soldaplication/Activities/LoadingActivity.java
+++ app/src/main/java/com/example/soldaplication/Activities/LoadingActivity.java
@@ -1,13 +1,14 @@
-package com.example.soldaplication;
+package com.example.soldaplication.Activities;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import com.example.soldaplication.R;
 
 public class LoadingActivity extends AppCompatActivity {
     Thread splashTread;
--- app/src/main/java/com/example/soldaplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/soldaplication/Activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.soldaplication;
+package com.example.soldaplication.Activities;
 
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.soldaplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
