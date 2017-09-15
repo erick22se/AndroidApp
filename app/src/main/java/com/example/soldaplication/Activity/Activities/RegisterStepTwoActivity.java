d07ce50a69bc0ec7cd0db67b361ba6e41cd79cf2
erick22se <erick1798CF@hotmail.com>
2017-09-15 15:15:29 -0500

    Fixing Structure of the package

---
+++ app/src/main/java/com/example/soldaplication/Activity/Activities/RegisterStepTwoActivity.java
@@ -0,0 +1,34 @@
+package com.example.soldaplication.Activity.Activities;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import com.example.soldaplication.Activity.Activities.MainActivity;
+import com.example.soldaplication.R;
+
+public class RegisterStepTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_step_two);
+        Button saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckBox confirmation = (CheckBox)findViewById(R.id.conditionCheckBox);
+                if(confirmation.isChecked())
+                    startActivity(new Intent(view.getContext(),MainActivity.class));
+                else
+                    Snackbar.make(view, "Confirm our terms and conditions", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+            }
+        });
+    }
+}
