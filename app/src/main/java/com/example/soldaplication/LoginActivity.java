51cb26d48fb51afd2406dd9d7756c758c87b73e2
erick22se <erick1798CF@hotmail.com>
2017-09-14 20:54:54 -0500

    Setting the register button

--- app/src/main/java/com/example/soldaplication/LoginActivity.java
+++ app/src/main/java/com/example/soldaplication/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.example.soldaplication;
 
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText emailEditText;
+    TextInputEditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailEditText =(TextInputEditText)findViewById(R.id.emailTextInputEdit);
+        passwordEditText =(TextInputEditText)findViewById(R.id.passwordTextInputEdit); Button button = (Button)findViewById(R.id.enter_Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(emailEditText.getText().equals("sample@hotmail.com")&&passwordEditText.getText().equals("123"))
+                    //startActivity(new Intent(view.getContext(),LoginActivity.class));
+                    Snackbar.make(view, "Your email or password are wrong", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                else
+                    Snackbar.make(view, "Your email or password are wrong", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+            }
+        });
     }
 }
