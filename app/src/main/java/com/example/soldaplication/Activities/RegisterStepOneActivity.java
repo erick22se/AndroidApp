45a09779e94505d5ec0efe4a1a2554f7726caa34
erick22se <erick1798CF@hotmail.com>
2017-09-15 14:31:18 -0500

    Advance of the RedisterStepTwo.xml

--- app/src/main/java/com/example/soldaplication/Activities/RegisterStepOneActivity.java
+++ app/src/main/java/com/example/soldaplication/Activities/RegisterStepOneActivity.java
@@ -1,7 +1,10 @@
 package com.example.soldaplication.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.soldaplication.R;
 
@@ -11,5 +14,12 @@ public class RegisterStepOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_step_one);
+        Button nextButton = (Button)findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),RegisterStepTwoActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/soldaplication/Activities/RegisterStepTwoActivity.java
@@ -0,0 +1,15 @@
+package com.example.soldaplication.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.soldaplication.R;
+
+public class RegisterStepTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_step_two);
+    }
+}
