ff7ba565e63edff3378dc06f4eb6a1e7d3e74343
erick22se <erick1798CF@hotmail.com>
2017-09-15 00:06:27 -0500

    Finshing step one register

--- app/src/main/java/com/example/soldaplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/soldaplication/Activities/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_myAuctions);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_store);
