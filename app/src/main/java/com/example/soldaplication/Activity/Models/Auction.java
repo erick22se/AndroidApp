68bac31cc2525b334b8bcc9c1a67839551e89c4b
erick22se <erick1798CF@hotmail.com>
2017-09-17 20:56:29 -0500

    User class Added

---
+++ app/src/main/java/com/example/soldaplication/Activity/Models/Auction.java
@@ -0,0 +1,20 @@
+package com.example.soldaplication.Activity.Models;
+
+import java.util.Date;
+import java.util.Timer;
+
+/**
+ * Created by Erick_lap on 17/09/2017.
+ */
+
+public class Auction {
+    private String id;
+    private String tittle;
+    private String description;
+    private Date duration;
+    private float priceBase;
+    private float priceWinner;
+    private String address;
+
+
+}
